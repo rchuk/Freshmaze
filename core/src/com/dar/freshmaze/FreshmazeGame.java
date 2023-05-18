ae024c2be889dba06d091cda00828f4bfc7360fb
Ruslan Omelchuk <romelchuk12@gmail.com>
2023-05-18 22:35:18 +0300

    Removed example code

--- core/src/com/dar/freshmaze/FreshmazeGame.java
+++ core/src/com/dar/freshmaze/FreshmazeGame.java
@@ -1,31 +1,40 @@
 package com.dar.freshmaze;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class FreshmazeGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private final static float WIDTH = 640;
+	private final static float HEIGHT = 480;
+
+	private OrthographicCamera camera;
+	private FitViewport viewport;
+
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		viewport = new FitViewport(WIDTH, HEIGHT, camera);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		camera.update();
+
+		ScreenUtils.clear(0.1f, 0.1f, 0.25f, 1.0f);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		viewport.update(width, height);
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
