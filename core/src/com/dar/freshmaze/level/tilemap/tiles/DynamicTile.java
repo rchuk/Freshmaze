e8b8fbbd23f8cbcfaa0fb5f7904774a97eff170e
Ruslan Omelchuk <romelchuk12@gmail.com>
2023-06-01 17:49:50 +0300

    Created DynamicTile class

---
+++ core/src/com/dar/freshmaze/level/tilemap/tiles/DynamicTile.java
@@ -0,0 +1,35 @@
+package com.dar.freshmaze.level.tilemap.tiles;
+
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.dar.freshmaze.level.tilemap.LevelTilemap;
+
+public abstract class DynamicTile {
+    private final LevelTilemap tilemap;
+    private final LevelTilemap.CellPos cellPos;
+    private final TiledMapTile defaultTile;
+
+    protected Body physBody;
+
+    public DynamicTile(LevelTilemap tilemap, LevelTilemap.CellPos cellPos, TiledMapTile defaultTile) {
+        this.tilemap = tilemap;
+        this.cellPos = cellPos;
+        this.defaultTile = defaultTile;
+    }
+
+    public LevelTilemap getTilemap() {
+        return tilemap;
+    }
+
+    public Body getPhysBody() {
+        return physBody;
+    }
+
+    public LevelTilemap.CellPos getCellPos() {
+         return cellPos;
+    }
+
+    public TiledMapTile getDefaultTile() {
+        return defaultTile;
+    }
+}
